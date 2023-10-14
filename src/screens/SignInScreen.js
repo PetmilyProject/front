7e41ea8e510b5fd3abc10aac06ac3087e174c3d7
yoo-hwan <youh5325@naver.com>
2023-10-14 15:53:29 +0900

    1014수정

--- src/screens/SignInScreen.js
+++ src/screens/SignInScreen.js
@@ -1,130 +1,13 @@
-// import {
-//   Button,
-//   Keyboard,
-//   KeyboardAvoidingView,
-//   StyleSheet,
-//   Text,
-//   TouchableWithoutFeedback,
-//   View,
-// } from 'react-native';
-
-// import Input, {
-//   InputTypes,
-//   KeyboardTypes,
-//   ReturnKeyTypes,
-// } from '../components/Input';
-// import { useRef, useState } from 'react';
-// import axios from 'axios';
-// import AsyncStorage from '@react-native-async-storage/async-storage';
-// import SquareButton, { ColorTypes } from '../components/Button';
-// import { useContext } from 'react';
-// import { AuthContext } from '../navigations/Nest';
-// import { WHITE } from '../colors';
-
-// const SignInScreen = () => {
-//   const [email, setEmail] = useState('');
-//   const [password, setPassword] = useState('');
-
-//   const { signIn, isSignedIn } = useContext(AuthContext); // isSignedIn 가져오기
-
-//   const handleSignIn = () => {
-//     AsyncStorage.setItem('email', email);
-//     axios
-//       .post(
-//         'http://ec2-43-200-8-47.ap-northeast-2.compute.amazonaws.com:8080/auth/login',
-//         {
-//           email: email,
-//           password: password,
-//         }
-//       )
-//       .then((response) => {
-//         const token = response.data.token;
-//         const loginsuccess = () => {
-//           setIsSignedIn(true);
-//         };
-
-//         AsyncStorage.setItem('token', token)
-//           .then(() => {
-//             console.log('로그인 토큰 저장 완료:', token);
-
-//             // 다음 화면으로 이동
-//             signIn();
-//           })
-//           .catch((error) => {
-//             console.error('로그인 토큰 저장 실패:', error);
-//           });
-//       })
-//       .catch((error) => {
-//         console.error('로그인 실패:', error);
-//       });
-//     //signIn();
-//   };
-
-//   const dismissKeyboard = () => {
-//     Keyboard.dismiss();
-//   };
-
-//   return (
-//     <TouchableWithoutFeedback onPress={dismissKeyboard}>
-//       <KeyboardAvoidingView style={styles.container} behavior="padding">
-//         <Input
-//           styles={{
-//             container: { marginBottom: 20, paddingHorizontal: 20 },
-//             input: { borderWidth: 1 },
-//           }}
-//           value={email}
-//           onChangeText={(text) => setEmail(text.trim())}
-//           inputType={InputTypes.EMAIL}
-//           returnKeyType={ReturnKeyTypes.NEXT}
-//         />
-//         <Input
-//           styles={{
-//             container: { marginBottom: 20, paddingHorizontal: 20 },
-//             input: { borderWidth: 1 },
-//           }}
-//           value={password}
-//           onChangeText={(text) => setPassword(text.trim())}
-//           inputType={InputTypes.PASSWORD}
-//           returnKeyType={ReturnKeyTypes.DONE}
-//         />
-//         <SquareButton
-//           colorType={ColorTypes.YELLOW}
-//           text="로그인하기"
-//           onPress={handleSignIn}
-//         />
-//       </KeyboardAvoidingView>
-//     </TouchableWithoutFeedback>
-//   );
-// };
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     backgroundColor: WHITE,
-//   },
-// });
-
-// export default SignInScreen;
-
 import React from 'react';
 import {
-  Button,
   Keyboard,
   KeyboardAvoidingView,
   StyleSheet,
-  Text,
   TouchableWithoutFeedback,
-  View,
 } from 'react-native';
 
-import Input, {
-  InputTypes,
-  KeyboardTypes,
-  ReturnKeyTypes,
-} from '../components/Input';
-import { useRef, useState } from 'react';
+import Input, { InputTypes, ReturnKeyTypes } from '../components/Input';
+import { useState } from 'react';
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import SquareButton, { ColorTypes } from '../components/Button';
